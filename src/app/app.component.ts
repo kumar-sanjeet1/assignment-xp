a439143cc46bc7e2cc90eb96c27468ecf31a3a4d
Sanjeet <Cfe.sanjeet@mettl.com>
2018-08-01 18:27:07 +0530

    assignment completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  listItems = [0];
+  fontSize = 16;
+
+
+  addItem() {
+    const len = this.listItems.length;
+    this.listItems.push(len);
+  }
+
+  deleteItem(index) {
+    this.listItems.splice(index, 1);
+  }
+
+  updateFont(event) {
+    this.fontSize = event.target.value;
+    console.log(event.target.value);
+  }
 }
